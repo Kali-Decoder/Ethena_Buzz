341829c197ef2d3c4a12f84384dfb512fcb72f75
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2024-11-24 01:32:38 +0530

    deploy script

---
+++ layer-zero/scripts/deploy_oft_adapter.ts
@@ -0,0 +1,31 @@
+import { ethers } from "hardhat";
+
+const OFTAdapter_CONTRACT_NAME = process.env.OFTAdapter_CONTRACT_NAME || "MyOFTAdapter";
+
+async function deployOFTAdapter(erc20TokenAddress: string, lzEndpointOnSrcChain: string) {
+  const contractOwner: string = await ethers.getSigners().then(res => res[0].address);
+  console.log("Deploying OFTAdapter contract with the following parameters:", contractOwner, erc20TokenAddress, lzEndpointOnSrcChain);
+  const myOFTAdapterContract = await ethers.deployContract(OFTAdapter_CONTRACT_NAME, [
+    erc20TokenAddress,
+    lzEndpointOnSrcChain,
+    contractOwner,
+  ]);
+  await myOFTAdapterContract.waitForDeployment();
+
+  console.log("Deployed OFTAdapter contract address:", await myOFTAdapterContract.getAddress());
+}
+
+async function main() {
+  const { erc20TokenAddress, lzEndpointOnSrcChain } = process.env;
+  if (!erc20TokenAddress) {
+    throw new Error("Missing erc20TokenAddress");
+  } else if (!lzEndpointOnSrcChain) {
+    throw new Error("Missing lzEndpointOnSrcChain");
+  }
+  await deployOFTAdapter(erc20TokenAddress, lzEndpointOnSrcChain);
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
