ed7f14bfdb763dc3747bcf7c914506175b3baea6
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-01-30 13:19:13 +0530

    Update

--- client/src/app/launch/_components/post_component.tsx
+++ client/src/app/launch/_components/post_component.tsx
@@ -1,87 +1,84 @@
 import { useDataContext } from "@/context/DataContext";
 import React from "react";
-
-const PostComponent: React.FC = ({item}) => {
-    const {openSideBar,setActivePoolId} = useDataContext();
-    return <>
-        <div className="twitter-post" onClick={()=>{
-            setActivePoolId(item?.id);
-            openSideBar();
-        }}>
-            <div className="twitter-post__retweeted">
-                <svg
-                    viewBox="0 0 24 24"
-                    className="r-9ilb82 r-4qtqp9 r-yyyyoo r-1yevf0r r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-meisx5"
-                >
-                    <g>
-                        <path
-                            d="M18.265 3.314c-3.45-3.45-9.07-3.45-12.52 0-3.45 3.44-3.45 9.06 0 12.51 1.5 1.49 3.43 2.38 5.51 2.56v4.14c0 .31.2.6.5.7.08.03.17.05.25.05.22 0 .44-.1.59-.29l6.49-8.11c2.63-3.49 2.27-8.47-.82-11.56zm-10.56 7.87c0-.41.33-.75.75-.75h4.05c.41 0 .75.34.75.75s-.34.75-.75.75h-4.05c-.42 0-.75-.34-.75-.75zm8.6-3.24c0 .42-.34.75-.75.75h-7.1c-.42 0-.75-.33-.75-.75 0-.41.33-.75.75-.75h7.1c.41 0 .75.34.75.75z"
-                        ></path>
-                    </g>
-                </svg>
-                <span className="uppercase text-blue-400">{item?.category}</span>
-            </div>
-            <div className="twitter-post__bottom mt-4">
-
-                <div className="twitter-post__content">
-                    <div className="twitter-post__title mb-2">
-                        <span className="name">@ {item?.name}</span>
-                        <span className="separator">-</span>
-                        <span className="date">7h</span>
-                    </div>
-                    <p className="twitter-post__paragraph">
-                    {item?.description}
-                    </p>
-                    <ul className="twitter-post__interactions">
-                        <li className="comments ">
-                            <span>$ {item?.total_amount}</span>
-                        </li>
-                        <li className="retweets">
-                            <svg
-                                viewBox="0 0 24 24"
-                                className="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
-                            >
-                                <g>
-                                    <path
-                                        d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"
-                                    ></path>
-                                </g>
-                            </svg>
-                            <span>{item?.total_bets}</span>
-                        </li>
-                        <li className="likes">
-                            <svg
-                                viewBox="0 0 24 24"
-                                className="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
-                            >
-                                <g>
-                                    <path
-                                        d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"
-                                    ></path>
-                                </g>
-                            </svg>
-                            <span>421</span>
-                        </li>
-                        <li className="share">
-                            <svg
-                                viewBox="0 0 24 24"
-                                className="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
-                            >
-                                <g>
-                                    <path
-                                        d="M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z"
-                                    ></path>
-                                    <path
-                                        d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"
-                                    ></path>
-                                </g>
-                            </svg>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-    </>
+import { FiDollarSign } from "react-icons/fi";
+import { HiUsers } from "react-icons/hi";
+import { RiTimerLine } from "react-icons/ri";
+import { RiRadioButtonLine } from "react-icons/ri";
+interface Post {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  total_amount: number;
+  total_bets: number;
+  finalScore: number;
+  startTime: number;
+  endTime: number;
+  resultDeclareTime: number;
+  poolEnded: boolean;
 }
+const PostComponent = ({ item }: { item: Post }) => {
+  const { openSideBar, setActivePoolId, formatTimestamp } = useDataContext();
+  return (
+    <>
+      <div
+        className="twitter-post"
+        onClick={() => {
+          setActivePoolId(+item?.id);
+          openSideBar();
+        }}
+      >
+        <div className="twitter-post__retweeted">
+          <svg
+            viewBox="0 0 24 24"
+            className="r-9ilb82 r-4qtqp9 r-yyyyoo r-1yevf0r r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-meisx5"
+          >
+            <g>
+              <path d="M18.265 3.314c-3.45-3.45-9.07-3.45-12.52 0-3.45 3.44-3.45 9.06 0 12.51 1.5 1.49 3.43 2.38 5.51 2.56v4.14c0 .31.2.6.5.7.08.03.17.05.25.05.22 0 .44-.1.59-.29l6.49-8.11c2.63-3.49 2.27-8.47-.82-11.56zm-10.56 7.87c0-.41.33-.75.75-.75h4.05c.41 0 .75.34.75.75s-.34.75-.75.75h-4.05c-.42 0-.75-.34-.75-.75zm8.6-3.24c0 .42-.34.75-.75.75h-7.1c-.42 0-.75-.33-.75-.75 0-.41.33-.75.75-.75h7.1c.41 0 .75.34.75.75z"></path>
+            </g>
+          </svg>
+          <span className="uppercase text-blue-400">{item?.category}</span>
+        </div>
+        <div className="twitter-post__bottom mt-4">
+          <div className="twitter-post__content">
+            <div className="twitter-post__title mb-2">
+              <span className="name">@ {item?.name} </span>
+                <span className="handle">#{item?.id} - </span>
+                <span> {formatTimestamp(item?.startTime)}</span>
+            </div>
+            <p className="twitter-post__paragraph">{item?.description}</p>
+            <ul className="twitter-post__interactions">
+              <li className="comments">
+                <FiDollarSign />
+                <span> {item?.total_amount}</span>
+              </li>
+              <li className="retweets">
+                <HiUsers />
+                <span>{item?.total_bets}</span>
+              </li>
+              <li className="likes">
+                <RiTimerLine />
+                <span>{formatTimestamp(item?.endTime)}</span>
+              </li>
+
+              <li className="retweets">
+                {item?.poolEnded ? (
+                  <>
+                    <RiRadioButtonLine className="text-red" />
+                    <span>ENDED</span>
+                  </>
+                ) : (
+                  <>
+                    <RiRadioButtonLine className="text-green-500" />
+                    <span>ONGOING</span>
+                  </>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default PostComponent;
--- client/src/app/launch/page.tsx
+++ client/src/app/launch/page.tsx
@@ -4,9 +4,10 @@ import PostComponent from "./_components/post_component";
 import postData from "./post_data.json";
 import RightSidebar from "./_components/sidebar";
 import { useAccount } from "wagmi";
+import Link from "next/link";
 
 const LaunchPage: React.FC = () => {
-  const {address} = useAccount();
+  const { address } = useAccount();
   return (
     <main className="twitter">
       <div className="twitter__left">
@@ -43,7 +44,7 @@ const LaunchPage: React.FC = () => {
           </nav>
           <div className="twitter-logged">
             <div className="twitter-logged-info">
-              {address?.slice(0,15)+ "..." + address?.slice(-5)}
+              {address?.slice(0, 15) + "..." + address?.slice(-5)}
             </div>
           </div>
         </aside>
@@ -82,20 +83,21 @@ const LaunchPage: React.FC = () => {
             ))}
           </div>
           <div className="w-[20%] mt-8">
-            <button
-              className="twitter-nav__item active"
-              onClick={() => alert("Hello World !!!")}
-            >
-              <svg
-                viewBox="0 0 24 24"
-                className="r-13gxpu9 r-4qtqp9 r-yyyyoo r-lwhw9o r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"
+            <Link href="/create">
+              <button
+                className="twitter-nav__item active"
               >
-                <g>
-                  <path d="M22.58 7.35L12.475 1.897c-.297-.16-.654-.16-.95 0L1.425 7.35c-.486.264-.667.87-.405 1.356.18.335.525.525.88.525.16 0 .324-.038.475-.12l.734-.396 1.59 11.25c.216 1.214 1.31 2.062 2.66 2.062h9.282c1.35 0 2.444-.848 2.662-2.088l1.588-11.225.737.398c.485.263 1.092.082 1.354-.404.263-.486.08-1.093-.404-1.355zM12 15.435c-1.795 0-3.25-1.455-3.25-3.25s1.455-3.25 3.25-3.25 3.25 1.455 3.25 3.25-1.455 3.25-3.25 3.25z"></path>
-                </g>
-              </svg>
-              <span>Create New</span>
-            </button>
+                <svg
+                  viewBox="0 0 24 24"
+                  className="r-13gxpu9 r-4qtqp9 r-yyyyoo r-lwhw9o r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"
+                >
+                  <g>
+                    <path d="M22.58 7.35L12.475 1.897c-.297-.16-.654-.16-.95 0L1.425 7.35c-.486.264-.667.87-.405 1.356.18.335.525.525.88.525.16 0 .324-.038.475-.12l.734-.396 1.59 11.25c.216 1.214 1.31 2.062 2.66 2.062h9.282c1.35 0 2.444-.848 2.662-2.088l1.588-11.225.737.398c.485.263 1.092.082 1.354-.404.263-.486.08-1.093-.404-1.355zM12 15.435c-1.795 0-3.25-1.455-3.25-3.25s1.455-3.25 3.25-3.25 3.25 1.455 3.25 3.25-1.455 3.25-3.25 3.25z"></path>
+                  </g>
+                </svg>
+                <span>Create New</span>
+              </button>
+            </Link>
           </div>
         </div>
 
@@ -113,101 +115,6 @@ const LaunchPage: React.FC = () => {
         </div>
       </div>
 
-      {/* <div className="twitter__right">
-                    <div className="twitter-trendings" data-aos="fade-left">
-                        <ul className="twitter-trendings__list">
-                            <span className="twitter-trendings__list-title"
-                            >Make your Prediction</span>
-                            <li className="twitter-trendings-item">
-                                <div className="left">
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >Assunto do Momento no Brasil</span
-                                    >
-                                    <span className="twitter-trendings-item__title">ROXA</span>
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >12.2 mil tweets</span
-                                    >
-                                </div>
-                            </li>
-                            <li className="twitter-trendings-item">
-                                <div className="left">
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >Futebol · Assunto do Momento</span
-                                    >
-                                    <span className="twitter-trendings-item__title">Eduardo Sasha</span>
-                                </div>
-                            </li>
-                            <li className="twitter-trendings-item">
-                                <div className="left">
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >Covid-19 - AO VIVO</span
-                                    >
-                                    <span className="twitter-trendings-item__title"
-                                    >Governo faz apelo à China para ter 30 milhões de doses e cita
-                                        risco de parar vacinação</span
-                                    >
-                                </div>
-                                <span
-                                    style={{
-                                        backgroundImage: `url(https://pbs.twimg.com/semantic_core_img/1369458980817281024/wSIJlqDR?format=jpg&name=240x240)`
-                                    }}
-                                    className="twitter-trendings-item__img"
-                                ></span>
-                            </li>
-                            <li className="twitter-trendings-item twitter-trendings-item--showmore">
-                                <span>Mostrar mais</span>
-                            </li>
-                        </ul>
-
-                    </div>
-                    <div className="twitter-trendings" data-aos="fade-left">
-                        <ul className="twitter-trendings__list">
-                            <span className="twitter-trendings__list-title"
-                            >Make your Prediction</span>
-                            <li className="twitter-trendings-item">
-                                <div className="left">
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >Assunto do Momento no Brasil</span
-                                    >
-                                    <span className="twitter-trendings-item__title">ROXA</span>
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >12.2 mil tweets</span
-                                    >
-                                </div>
-                            </li>
-                            <li className="twitter-trendings-item">
-                                <div className="left">
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >Futebol · Assunto do Momento</span
-                                    >
-                                    <span className="twitter-trendings-item__title">Eduardo Sasha</span>
-                                </div>
-                            </li>
-                            <li className="twitter-trendings-item">
-                                <div className="left">
-                                    <span className="twitter-trendings-item__subtitle"
-                                    >Covid-19 - AO VIVO</span
-                                    >
-                                    <span className="twitter-trendings-item__title"
-                                    >Governo faz apelo à China para ter 30 milhões de doses e cita
-                                        risco de parar vacinação</span
-                                    >
-                                </div>
-                                <span
-                                    style={{
-                                        backgroundImage: `url(https://pbs.twimg.com/semantic_core_img/1369458980817281024/wSIJlqDR?format=jpg&name=240x240)`
-                                    }}
-                                    className="twitter-trendings-item__img"
-                                ></span>
-                            </li>
-                            <li className="twitter-trendings-item twitter-trendings-item--showmore">
-                                <span>Mostrar mais</span>
-                            </li>
-                        </ul>
-
-                    </div>
-
-                </div> */}
 
       <RightSidebar />
     </main>
