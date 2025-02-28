a3cc5090fecb40e37e51afa07402fad81bd1297f
sneha gupta <snehagupta98930@gmail.com>
2025-03-01 04:23:05 +0530

    added all buzzify apis

---
+++ server/dao/user.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    address: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
