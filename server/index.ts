9e6f46d65a2760199c4c50c67472a3243c3d1f82
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-03-02 03:35:16 +0530

    update server

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://ethena-buzz.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
